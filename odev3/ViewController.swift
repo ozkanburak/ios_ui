507911aa84356ee0ae075e0b19acdc27ddaee5f4
Burak Özkan <ozkanburaak@gmail.com>
2022-08-19 23:43:43 +0300

    ios_arayüz

--- odev3/ViewController.swift
+++ odev3/ViewController.swift
@@ -11,7 +11,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.navigationItem.title = "Dietapp"
+        
+        let appearence = UINavigationBarAppearance()
+        
+        appearence.backgroundColor = UIColor(named: "anaRenk")
+        appearence.titleTextAttributes = [.foregroundColor: UIColor(named: "textRenk1")!, .font: UIFont(name: "Pacifico-Regular", size: 22)!]
+       
+        // bir çok text denedim sürekli hata verdi
+        
+        navigationController?.navigationBar.barStyle = .black
+        
+        navigationController?.navigationBar.standardAppearance = appearence
+        navigationController?.navigationBar.compactAppearance = appearence
+        navigationController?.navigationBar.scrollEdgeAppearance = appearence
+        
     }
 
 
